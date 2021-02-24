8b02b7bd042a1eab99bbbcd4ad3ec98bc284102c
Denis <den-sidnay@yandex.ru>
2021-02-24 22:51:25 +0300

    add 12 lesson

--- extends/lesson12/Enumerable.php
+++ extends/lesson12/Enumerable.php
@@ -7,7 +7,28 @@ trait Enumerable
     // BEGIN (write your solution here)
     public function maxBy($fn)
     {
-        
+        $arr = $this->getIterator();
+        if (count($arr) === 0) {
+            return null;
+        }
+        $res = array_reduce($arr, function ($acc, $item) {
+            return $item->getDuration() > $acc->getDuration() ? $item : $acc;
+        }, $arr[0]);
+        return $res;
     }
     // END
 }
+
+// hexlet solution
+// public function maxBy(callable $fn)
+// {
+//     $items = $this->getIterator();
+//     if (!count($items)) {
+//         return null;
+//     }
+//     $result = array_reduce($items, function ($acc, $item) use ($fn) {
+//          $value = $fn($acc, $item);
+//          return $value >= 0 ? $acc : $item;
+//     }, $items[0]);
+//     return $result;
+// }
--- extends/lesson12/index.php
+++ extends/lesson12/index.php
@@ -1,2 +1,19 @@
 <?php
 
+require_once('Course.php');
+require_once('Lesson.php');
+
+$lessons = [
+    // Второй параметр это продолжительность урока в минутах
+    new Lesson('react start', 3),
+    new Lesson('react component', 9),
+    new Lesson('react lifecycle', 2),
+    new Lesson('redux', 4),
+];
+ 
+// use Enumerable;
+$course = new Course($lessons);
+echo "<pre>";
+print_r($course->getIterator());
+print_r($course->maxBy('fh'));
+echo "</pre>";
